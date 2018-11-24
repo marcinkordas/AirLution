30c2207c604783fde3fb057a14b4126e3af0067a
Kamil Sokolowski <ksokolowski95@gmail.com>
2018-11-24 16:56:21 +0100

    Build changed

---
+++ TestMQTTBroker/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TestMQTTBroker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MQTT Broker is gong to start");
+            MQTTBroker.StartBroker();
+
+
+            Console.ReadLine();
+        }
+    }
+}
